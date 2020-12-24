9573f14c6aea42721442f46a911040d207df118c
2271277962 <55794465+2271277962@users.noreply.github.com>
2020-12-24 14:48:37 +0800

    Update python.py

--- python.py
+++ python.py
@@ -2,10 +2,11 @@ import requests
 headers = {
         'user-agent':'Mozilla/5.0 (Linux; Android 10; Redmi K30 Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/85.0.4183.127 Mobile Safari/537.36',
         ##    下面填抓包来的参数########
-        'Authorization':'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2NsYWltcyI6eyJzYWx0IjoxMDAsIm51bWJlciI6NTYyMDk1Njk5LCJ0aW1lc3RhbXAiOjE2MDQ0OTE3ODV9LCJpZGVudGl0eSI6IjVkZTIxMWIyZmZhMDNjMGZkNTA4MjRiMSIsImp0aSI6IjkzMTA5NWViLWVkNzctNGJhMC1iMjdjLWY1YjUxNTQwOTYwNCIsImlhdCI6MTYwNDQ5MTc4NSwibmJmIjoxNjA0NDkxNzg1LCJmcmVzaCI6ZmFsc2UsInR5cGUiOiJhY2Nlc3MifQ.hvacTOwTmXWiNiKMJ_O66x-ujEtj6mhTC2R1aZ1ronE'
+        'Authorization':''
         }
 #####Server酱的sckey填这里，不需要可以留空####
-server_key = 'SCU102384Tbda8eb6ed4bf8ec01eb04dd3adc11ff95eef779fd44c1'
+server_key = ''
+
 def qiandao(headers):
     url = 'http://n.cg.163.com/api/v2/sign-today'
     r = requests.post(url,headers=headers).text
